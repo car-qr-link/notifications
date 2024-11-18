932ddca7368b60d75ab795b76268bdeebafeb49f
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-11-18 15:06:45 +0700

    [redis] connect Redis as broker and storage

---
+++ src/config/storage.config.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class StorageConfig {
+  public readonly url: string;
+
+  constructor(configService: ConfigService) {
+    this.url = configService.get(
+      'STORAGE__URL',
+      'redis://localhost:6379/0',
+    );
+  }
+}
--- src/core/notifications/notifications.service.ts
+++ src/core/notifications/notifications.service.ts
@@ -1,13 +1,21 @@
-import { notification } from '@car-qr-link/apis';
-import { Injectable } from '@nestjs/common';
+import { messaging, notification } from '@car-qr-link/apis';
+import { Injectable, Logger } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import { EVENT_MESSAGE_RECEIVED } from '../messaging/messaging.events';
 
 @Injectable()
 export class NotificationsService {
+    private readonly logger = new Logger(NotificationsService.name);
+
     async selectReasons(): Promise<notification.Reason[]> {
         return REASONS;
     }
 
 
+    @OnEvent(EVENT_MESSAGE_RECEIVED)
+    async onAnswer(payload: messaging.MessageReceived): Promise<void> {
+
+    }
 }
 
 const REASONS: notification.Reason[] = [
